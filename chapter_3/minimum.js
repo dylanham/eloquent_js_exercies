6894bcb248a32fd4719d4fcbad40ee96fa3922de
Dylan Ham <dylanham@gmail.com>
2015-04-19 07:45:50 -0600

    ch 3 exercise 1 minimum finished

---
+++ chapter_3/minimum.js
@@ -0,0 +1,13 @@
+function minimum(number1, number2) {
+  if (number1 < number2) {
+    console.log(number1);
+  } else {
+    console.log(number2);
+  }
+}
+
+minimum(3, 5)
+minimum(5, 4)
+minimum(0, 5)
+minimum(5, -1)
+minimum(8, 8)
