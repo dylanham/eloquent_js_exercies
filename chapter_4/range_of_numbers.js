79ebd5bf7c072261719f8b9b58a4ea2316e16b8e
Dylan Ham <dylanham@gmail.com>
2015-04-20 00:08:17 -0600

    range of numbers and sum exercise done

--- chapter_4/range_of_numbers.js
+++ chapter_4/range_of_numbers.js
@@ -1,11 +1,16 @@
-function range(start, end) {
-  for (i = start, results = []; i <= end; i++) {
+function range(start, end, step) {
+  for (i = start, results = []; i <= end;) {
     results.push(i);
+    if (step != null) {
+      i += step
+    } else {
+      i++;
+    }
   }
   return results;
 }
 
-console.log(range(1, 10))
+console.log(range(1, 10, 3))
 
 function sum(arrayOfNumbers) {
   for (i = 0, results = 0; i < arrayOfNumbers.length; i++) {
