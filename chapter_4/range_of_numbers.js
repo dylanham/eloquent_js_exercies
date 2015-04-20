e49f6a11ecd699c19ef08ee86e66677bfbbe7fe6
Dylan Ham <dylanham@gmail.com>
2015-04-19 23:51:59 -0600

    add sum function

--- chapter_4/range_of_numbers.js
+++ chapter_4/range_of_numbers.js
@@ -1,8 +1,17 @@
-function rangeIt(start, end) {
+function range(start, end) {
   for (i = start, results = []; i <= end; i++) {
-    results.push(i)
+    results.push(i);
   }
-  console.log(results)
+  return results;
 }
 
-rangeIt(5, 100)
+console.log(range(1, 10))
+
+function sum(arrayOfNumbers) {
+  for (i = 0, results = 0; i < arrayOfNumbers.length; i++) {
+    results += arrayOfNumbers[i];
+  }
+  return results;
+}
+
+console.log(sum(range(1, 10)));
