ec8ba43616cc7a211825b5e3580bb2d7fdb884ca
Dylan Ham <dylanham@gmail.com>
2015-04-21 09:33:37 -0600

    a list halfway

--- chapter_4/a_list.js
+++ chapter_4/a_list.js
@@ -1,11 +1,24 @@
-function arrayToList(array) {
-  for (var node = list; node; node = node.rest) {
-    list = {
-      value: array[i]
-      rest: list }
+var arrayToList = function(array) {
+  for (var i = array.length -1; i >= 0; i--) {
+    if (i === array.length -1) {
+      var list = {value: array[i], rest: null};
+    } else {
+      var list = {value: array[i], rest: list};
+    }
   }
-  console.log(someList)
+  return list
+}
+
+var listToArray = function(list) {
+  array = []
+  for (var hash = list; hash; hash = hash.rest) {
+    array.push(hash.value);
+  }
+  return array
 }
 
 
 console.log(arrayToList([10, 20]));
+console.log(arrayToList([10, 20, 30]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(nth(arrayToList([10, 20, 30]), 1));
--- chapter_4/reversing_an_array.js
+++ chapter_4/reversing_an_array.js
@@ -1,5 +1,4 @@
-solution one
-function reverseArray(array) {
+var reverseArray = function(array) {
   var results = [];
   for (var i = array.length -1; i >= 0; i--) {
     results.push(array[i])
@@ -7,12 +6,17 @@ function reverseArray(array) {
   return results
 }
 
-function reverseArray(array) {
-  var results = [];
-  for (var i = 0; i < array.length; i ++) {
-    results.push(array[i])
+var reverseArrayInPlace = function(array) {
+  for ( var i = 0; i < Math.floor(array.length/2); i++) {
+    var originalElement = array[i];
+    var swapElement = array[array.length - 1 - i]
+    array[i] = swapElement;
+    array[array.length - 1 - i] = originalElement;
   }
-  return results
+  return array
 }
 
-console.log(reverseArray(["A", "B", "C"]));
+console.log(reverseArray([1, 2, 3, 4, 5, 6]))
+console.log(reverseArray(["a", "b", "c", "d", "e", "f"]))
+console.log(reverseArrayInPlace([1,2,3,4,5,6]))
+console.log(reverseArrayInPlace(["a", "b", "c", "d", "e", "f"]))
